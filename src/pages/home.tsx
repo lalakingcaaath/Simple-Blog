9506770d77d5c552411863a93e2bbd98ffcfcb84
Jericho <piojericho@gmail.com>
2026-01-30 15:32:28 +0800

    Added navbar component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,9 @@
+import NavBar from "../components/navBar"
+
 export default function Home () {
     return (
         <div className="home-page">
+            <NavBar />
             <h1>Welcome to Jecho's Blog</h1>
             <p>This is a simple blog application.</p>
         </div>
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom"
 
 export default function Register() {
-    const navigate = useNavigate();
+    const navigate = useNavigate()
 
     return (
         <div className="register-page">
