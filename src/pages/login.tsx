5de801cf6a5bf115bf9453b162277aa0f23183b7
Jericho <piojericho@gmail.com>
2026-01-30 20:11:50 +0800

    Added backend for authentication

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,24 +1,64 @@
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import supabase from "../config/supabaseClient";
 
 export default function Login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const [message, setMessage] = useState("")
+
+    const handleSubmit = async (event: { preventDefault: () => void }) => {
+        event.preventDefault();
+        setMessage("");
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        });
+
+        if (error) {
+            setMessage("Error: " + error.message);
+            setEmail("");
+            setPassword("");
+            return;
+        }
+
+        if (data) {
+            navigate('/home');
+            return null;
+        }
+    }
+
     const navigate = useNavigate();
-    console.log(supabase);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <h1 className="text-3xl font-bold underline mb-5">Login Page</h1>
+            {message && <p className="mb-4 text-center">{message}</p>}
             <div className="border rounded-lg p-6 bg-white shadow-md w-96">
                 <div className="text-center mb-4">
                     <p>Please enter your credentials</p>
                 </div>
-                    <form action="#" method="get" className="flex flex-col gap-4">
+                    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
                         <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" className="border rounded-lg" required />
+                        <input
+                        onChange={(e) => setEmail(e.target.value)} 
+                        value={email}  
+                        type="email" id="email" name="email" 
+                        className="border rounded-lg p-2"
+                        required
+                        />
                         <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" name="password" className="border rounded-lg" required />
+                        <input 
+                        onChange={(e) => setPassword(e.target.value)} 
+                        value={password} 
+                        type="password" id="password" name="password" 
+                        className="border rounded-lg p-2" 
+                        required 
+                        />
                         <div className="flex justify-center mt-4 gap-5">
-                            <button type="button" onClick={() => navigate('/home')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">Login</button>
+                            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">Login</button>
                             <button type="button" onClick={() => navigate('/register')} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded cursor-pointer">Register</button>
                         </div>
                     </form>
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,22 +1,72 @@
+import { useState } from "react"
+import supabase from "../config/supabaseClient"
 import { useNavigate } from "react-router-dom"
 
 export default function Register() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+    const [message, setMessage] = useState("")
+
+    const handleSubmit = async (event: { preventDefault: () => void }) => {
+        event.preventDefault();
+        setMessage("");
+
+        const { data, error } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        });
+
+        if (error) {
+            setMessage("Error: " + error.message);
+            return;
+        }
+
+        if (data) {
+            setMessage("Registration successful!");
+        }
+
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
+    }
+
     const navigate = useNavigate()
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <h1 className="text-3xl font-bold underline mb-5">Register Page</h1>
+            {message && <p className="mb-4 text-center">{message}</p>}
             <div className="border rounded-lg p-6 bg-white shadow-md w-96">
                 <div className="text-center mb-4">
                     <p>Please fill in the form to create an account</p>
                 </div>
-                    <form action="#" method="post" className="flex flex-col gap-4">
+                    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
                         <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" className="border rounded-lg" required />
+                        <input 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        value={email} 
+                        type="email" id="email" name="email"
+                        className="border rounded-lg p-2" 
+                        required 
+                        />
                         <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" name="password" className="border rounded-lg" required/>
+                        <input 
+                        onChange={(e) => setPassword(e.target.value)} 
+                        value={password} 
+                        type="password" id="password" name="password"
+                        className="border rounded-lg p-2" 
+                        required
+                        />
                         <label htmlFor="confirmPassword">Confirm Password:</label>
-                        <input type="password" id="confirmPassword" name="confirmPassword" className="border rounded-lg" required />
+                        <input 
+                        onChange={(e) => setConfirmPassword(e.target.value)} 
+                        value={confirmPassword} 
+                        type="password" id="confirmPassword" name="confirmPassword" 
+                        className="border rounded-lg p-2" 
+                        required 
+                        />
                         <div className="flex justify-center mt-4 gap-5">
                             <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">Register</button>
                             <button type="button" onClick={() => navigate('/login')} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded cursor-pointer">Already have an account?</button>
